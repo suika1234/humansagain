fbd1bd6d482e4cd7f6ec6fe247c92f1bdaadfce7
Erika Pilpre <erikapilpre@Erikas-MacBook-Pro.local>
2025-12-17 13:32:03 +0900

    Updates

---
+++ _next/static/cw6zPXpENmsJ3g1oT-njs/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/humansagain//_next/:path+",
+        "destination": "/humansagain/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
